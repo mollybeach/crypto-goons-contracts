aef2b5a476f97c9d7bdb523656011c07bd36ba62
Molly Beach <mollyjbeach@gmail.com>
2022-09-22 11:55:08 -0700

    Update pooltwo_arguments.js

--- pooltwo_arguments.js
+++ pooltwo_arguments.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const deployments = require('./data/deployments');
-
+//exports
 module.exports = [
     deployments.stakedPupzMainnet,
     deployments.stackv2LpTokenMainnet
