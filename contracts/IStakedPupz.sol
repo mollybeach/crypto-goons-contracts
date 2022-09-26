f6a62a20a30ee68ed6c613866dfc888ee0fa5359
Molly Beach <mollyjbeach@gmail.com>
2022-09-26 09:43:56 -0700

    Update IStakedPupz.sol

--- contracts/IStakedPupz.sol
+++ contracts/IStakedPupz.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.0;
 
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
-
+// IStakedPupz interface
 interface IStakedPupz is IERC20 {
     function mint(address account, uint256 amount) external;
 }
